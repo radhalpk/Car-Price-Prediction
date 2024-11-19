f596116353801d0ae2f16259f88698e9251ee91f
radhalpk <pavanradhala@gmail.com>
2024-11-20 01:19:03 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-
+"for administrative tasks"
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CarPricePrediction.settings')
